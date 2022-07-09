349f64a6e06d415e814275e217f903adf0b8ea2b
Fabricio assuncao <fabricioassuncao855@outlook.com>
2022-07-09 01:24:58 -0300

    models corrigido e melhorado

--- core_main/models.py
+++ core_main/models.py
@@ -1,33 +1,23 @@
 from msilib.schema import Class
 from pyexpat import model
 from django.db import models
-
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
-from models import Bicho
+
 
 
 # Create your models here.
 class Aposta(models.Model):
-
-    bicho_id = models.ForeignKey(Bicho.id)
-
-    data = models.DateField(
-        auto_now=True,
-    )
-
-    usuario_id = models.ForeignKey(User.id)
-
-    resultado = models.BooleanField(
-        default=False,)
+    bicho = models.ForeignKey('Bicho', on_delete=models.DO_NOTHING)
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resultado = models.BooleanField(default=False)
 
 class Bicho(models.Model):
-
     nome = models.CharField(
         verbose_name='Nome do Bicho',
         max_length= 32,
     )
-
     grupo = ArrayField(models.IntegerField())
 
     
