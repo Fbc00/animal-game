eff7b417dc9a594d5ee1cbf8eaa450ec6d173d4e
Fabricio assuncao <fabricioassuncao855@outlook.com>
2022-07-26 23:30:25 -0300

    apscheduler configurado

--- core_main/services/cron.py
+++ core_main/services/cron.py
@@ -2,7 +2,7 @@ from core_main.services import scripts
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
-@sched.scheduled_job('interval', hour=5, timezone="America/Sao_Paulo")
+@sched.scheduled_job('interval', hour=4, timezone="America/Sao_Paulo")
 def cron_sorteio_por_semana():
     scripts.desativaSorteio()
     scripts.gera_sorteios()
